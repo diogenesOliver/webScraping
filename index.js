737f6f66108fabd04a07ef82aa17eca90f0b00f9
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-12-11 11:02:48 -0300

    feat: Creating a json file with data coleted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { default: axios } = require('axios')
 const axio = require('axios')
 const cheerio = require('cheerio')
+const fs = require('fs')
 
 const PAGE_URL = "https://pt.wikipedia.org/wiki/Oscar_de_melhor_filme"
 
@@ -9,16 +10,26 @@ async function getMovies() {
     const $ = cheerio.load(data)
 
     const listOfMovies = []
+
     $('.wikitable tbody tr').each((i, element) => {
         const name = $(element).find('td[style*="background:#FAEB86"]').last().text().replace("\n", "")
         if (name !== '') {
             const year = $(element).find('td[style*="background:#FAEB86"]').first().prev('td').text().slice(-5).replace("\n", "")
             movie = { name, year }
             listOfMovies.push(movie)
-
-            console.log(listOfMovies)
         }
+    })
 
+    const formatedToStringfy = JSON.stringify(listOfMovies)
+    generatingAJSONFile(formatedToStringfy)
+}
+
+async function generatingAJSONFile(array){
+    fs.writeFileSync('data.json', array, err => {
+        if(err)
+            throw new Error()
+
+        return 'done .'
     })
 }
 
